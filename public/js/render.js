cb572b1ba09a926731f6b39f8d9bd818a72ab7ea
Boaz Yoo <blazeryoo1921@gmail.com>
2021-06-06 16:42:19 -0400

    icons + images

---
+++ public/js/render.js
@@ -0,0 +1,8 @@
+$(function(){
+  $("#nav_bar").load("/elements/nav_bar.html");
+  $("#sign_up").load("/elements/sign_up.html");
+  $("#fun_learning").load("/elements/fun_learning.html");
+  $("#about").load("/elements/about.html");
+  $("#subjects").load("/elements/subjects.html");
+  $("#color_banner").load("/elements/color_banner.html");
+});
