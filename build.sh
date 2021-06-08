5cd84e624bc365d62e748431ae76dea4c6cbdee0
Boaz Yoo <blazeryoo1921@gmail.com>
2021-06-08 17:06:38 -0400

    build static images & icons

--- build.sh
+++ build.sh
@@ -8,8 +8,10 @@ fi
 mkdir build
 cd build
 cp -r ../public/elements .
+cp -r ../public/Icons .
+cp -r ../public/Images .
+cp -r ../public/js .
 cp -r ../public/mybulma/sass .
 cp -r ../public/mybulma/css .
-cp -r ../public/js .
 cp ../public/index.html .
 echo "Build done"
